31ff73688fe1ebbd09134acda62f024a3ea7a4c0
pcpkomputer <terrificsubs@gmail.com>
2021-08-23 09:17:58 +0700

    feat: ...

--- src/pages/Training/PemesananTraining.js
+++ src/pages/Training/PemesananTraining.js
@@ -73,7 +73,7 @@ let settings = {
   let [selectedOffer, setSelectedOffer] = useState(0);
 
   
-  let [currentStep, setCurrentStep] = useState(2);
+  let [currentStep, setCurrentStep] = useState(4);
 
 
   useEffect(()=>{
@@ -262,7 +262,87 @@ let settings = {
                           </div>
                       </Col>
                   </Row>
-                  }        
+                  }       
+                  {
+                      (currentStep===3) &&
+                      <Row style={{marginTop:30}}>
+                      <Col lg={8} style={{marginBottom:50}}>
+                          <div style={{fontWeight:"bold",fontSize:20,marginBottom:60}}>Data Peserta</div>
+                          <Row style={{marginTop:20,justifyContent:"center",alignItems:"center"}}>
+                              <Col lg={4} style={{fontWeight:"bold",fontSize:17,marginBottom:10}}>
+                                  Nama Anda
+                              </Col>
+                              <Col lg={8} style={{paddingRight:10}}>
+                                  Padang Perwira Yudha
+                              </Col>
+                          </Row>
+                          <Row style={{marginTop:20,justifyContent:"center",alignItems:"center"}}>
+                              <Col lg={4} style={{fontWeight:"bold",fontSize:17,marginBottom:10}}>
+                                    Email
+                              </Col>
+                              <Col lg={8} style={{paddingRight:10}}>
+                                  padangperwirayudha@gmail.com
+                              </Col>
+                          </Row>
+                          <Row style={{marginTop:20,justifyContent:"center",alignItems:"center"}}>
+                              <Col lg={4} style={{fontWeight:"bold",fontSize:17,marginBottom:10}}>
+                                    No. Whatsapp
+                              </Col>
+                              <Col lg={8} style={{paddingRight:10}}>
+                                  089961989345
+                              </Col>
+                          </Row>
+                          <div style={{marginTop:40}}>
+                              <div style={{backgroundColor:"#e23b25",letterSpacing:1,color:"white",borderRadius:5,textAlign:"center",width:200,padding:"10px 15px 10px 15px"}}>
+                                  SELANJUTNYA
+                              </div>
+                          </div>
+                       
+                      </Col>
+                      <Col lg={4} style={{paddingLeft:40,paddingRight:40}}>
+                          <div style={{border:"solid 1px #e8e8e8",borderRadius:10,paddingTop:20,paddingBottom:20}}>
+                              <div style={{fontWeight:"bold",borderBottom:"solid 1px #e8e8e8",paddingBottom:20,marginRight:20,marginLeft:20}}>Pemesanan Anda</div>
+                              <div style={{marginTop:10,marginLeft:20,marginRight:20}}>
+                                  <div style={{display:"flex",marginBottom:20}}>
+                                        <div style={{flex:1}}>
+                                            <div style={{fontWeight:"bold",overflow:"hidden",textOverflow:"ellipsis",wordBreak:"break-word",paddingRight:20}}>Ahli K3 asdasdasdsadadasdsasadasdUmum Batcasdadadsadsadsadh 11asdsadasdassadasdasdasd5</div>
+                                            <div style={{fontSize:13,marginTop:10}}>28 Ag 2021</div>
+                                        </div>
+                                        <div style={{padding:5,display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                            <img src="https://mos.is3.cloudhost.id/photos/midiatama-58210629085720.png" style={{width:90,borderRadius:10}}></img>
+                                        </div>
+                                  </div>
+                                  
+                              </div>
+                              <div style={{marginLeft:20,marginRight:20,paddingTop:30,paddingBottom:30,borderTop:"solid 1px #e8e8e8",borderBottom:"solid 1px #e8e8e8"}}>
+                                  <div style={{fontWeight:"bold"}}>Kode Voucher</div>
+                                  <input type="text" value="" placeholder="Kode Voucher" readOnly={true} style={{marginTop:20,cursor:"not-allowed",padding:5,paddingLeft:15,fontSize:15,paddingRight:15, outline:"none",width:"100%",color:"grey",backgroundColor:"#e8e8e8",borderRadius:5,border:"solid 1px #f8f8f8"}}></input>
+                              </div>
+                              <div style={{marginTop:25,marginLeft:20,marginRight:20}}>
+                                  <div style={{fontWeight:"bold",marginBottom:10}}>Diskon</div>
+                                  <div style={{display:"flex",borderBottom:"solid 1px #e8e8e8",paddingBottom:30}}>
+                                      <div style={{flex:1}}>
+                                          <div style={{fontSize:15}}>Promo K3 Perusahan</div>
+                                      </div>
+                                      <div style={{width:100,textAlign:"right"}}>
+                                          <div style={{fontSize:15}}>-100000</div>
+                                      </div>
+                                  </div>
+                                  <div style={{marginTop:20,paddingBottom:55,display:"flex",justifyContent:"space-between"}}>
+                                        <div style={{fontWeight:"bold"}}>Jumlah Bayar</div>
+                                        <div>Rp. 60000000</div>
+                                  </div>
+                              </div>
+                          </div>
+                      </Col>
+                  </Row>
+                  }
+                  {
+                      (currentStep===4) &&
+                      <Row style={{marginTop:30}}>
+                          asdasd
+                      </Row>
+                  }
               </Container>
           </div>
           <Footer/>
